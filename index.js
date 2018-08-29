4580e339fde3e2d129a6231b5050bb935c8ea78e
eduardo <eduardobispofelizardo10@gmail.com>
2018-08-29 11:59:45 -0300

    iniciar o site

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const gatos = require('./data/gatos.json')
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('static'));
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.get('/gatos', (req, res) => {
+    res.render('gatos', {'gatos': gatos});
+});
+
+app.get('/sobre', (req, res) => {
+    res.render('sobre');
+});
+
+app.get('/styles.css', (req, res) => {
+    let cores = ['blue', 'red', 'yellow'];
+
+    let numero = Math.floor(Math.random() * 3);
+
+    let cor = cores[numero];
+    
+    res.send(`
+        body{
+            color: ${cor};
+        }
+    `);
+});
+
+app.listen(3000, () => {
+    console.log('Servidor inicializado')
+});
